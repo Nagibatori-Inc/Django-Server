7a20bbc7cc1aa6b91f16e2462e33230a8d1dbc2a
Daniil Lemeshko <gafret_kelp@outlook.com>
2025-02-18 22:06:52 +0300

    feat: implement base version of Profile model

--- auth/models.py
+++ auth/models.py
@@ -1,3 +1,25 @@
+from django.conf import settings
 from django.db import models
 
+
 # Create your models here.
+class Profile(models.Model):
+    """
+    Модель представляющая профиль пользователя
+
+    Fields:
+    + name (NULLABLE): Имя профиля, отображающееся в объявлениях, чатах, ЛК
+    + user: Связь с User'ом, который используется для аутентификации и авторизации
+    + type: Тип аккаунта, к примеру: Частное лицо или Компания
+    + is_deleted: Поле soft delete'a, выставляется на True в случае удаления со стороны пользователя
+    """
+
+    PROFILE_TYPE_CHOICES = {
+        "IND": "Частное лицо",
+        "CMP": "Компания"
+    }
+
+    name = models.CharField(max_length=50, null=True)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name="profile", on_delete=models.CASCADE)
+    type = models.CharField(max_length=3, choices=PROFILE_TYPE_CHOICES, default="IND")
+    is_deleted = models.BooleanField(default=False)
