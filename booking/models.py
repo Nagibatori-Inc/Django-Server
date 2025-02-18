96b44d789e98af027c42e95b31ce539a17139595
DocLivsey <doclivsey@github.com>
2025-02-18 15:27:05 +0300

    feat: init model class for user's adverts to contain it in db

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Advert(models.Model):
+    """
+    Модель объявления об аренде спецтехники
+    
+    Fields:
+    - category (Category): Категория объявления
+    - subcategory (Subcategory): Подкатегория 
+    + title (CharField): Название объявления
+    + description (TextField): Текст объявления
+    + price (DecimalField): Стоимость улсуги
+    - contact (Profile | Новая моделька о правовых Субъектах): Контакты - контакное лицо
+    + phone (Profile, но пока просто CharField): Контакты - телефон
+    + created_at (DateTimeField): Дата, когда было создано объявление
+    + activated_at (DateTimeField): Дата, когда пользователь активировал свое объявление
+    + is_active (BooleanField): Активно ли объявление
+    - promotion (Promotion): Данные о продвижении объявления
+    """
+    
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=11, decimal_places=2)
+    phone = models.CharField(length=12)
+    
+    created_at = models.DateTimeField(auto_now_add=True) # поле auto_now_add ставит datetime.now() когда объект только создан
+    activated_at = models.DateTimeField(auto_now=True) # поле auto_now задает значение datetime.now() когда у объект модели вызывает метод save()
+    is_active = models.BooleanField(default=False)
