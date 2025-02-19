5004075a1489f74ca7d9429098c0874f8445c53d
DocLivsey <doclivsey@github.com>
2025-02-19 13:15:29 +0300

    feat: added api view class for work (add, take, delete and change) with user's advert

--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from rest_framework.views import APIView
 
-# Create your views here.
+from booking.models import Advert
+
+
+class AdvertView(APIView):
+    def get(self, request):
+        advert_id = request.GET.get('id')
+        advert: Advert = get_object_or_404(Advert, pk=advert_id)
+        return advert
+    
+    def post(self, request):
+        pass
+    
+    def put(self, request):
+        pass
+    
+    def delete(self, request):
+        pass
