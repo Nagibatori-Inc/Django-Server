ca9a7c54efbf6fbaa6cd5b8494065e3e22927eb6
Daniil Lemeshko <gafret_kelp@outlook.com>
2025-02-20 22:36:19 +0300

    fix: remove required from type with default

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -20,11 +20,11 @@ class SignUpRequestSerializer(serializers.Serializer):
     # Аттрибуты для юзера
     phone = serializers.CharField(required=True, max_length=15)
     first_name = serializers.CharField(required=True)
-    email = serializers.EmailField(required=False, allow_blank=True)
+    email = serializers.EmailField(required=True, allow_blank=True)
     password = serializers.CharField(required=True)
     # Аттрибуты для профиля
     profile_name = serializers.CharField(required=False, max_length=50, allow_null=True)
-    type = serializers.ChoiceField(required=True, choices=Profile.PROFILE_TYPE_CHOICES, default="IND")
+    type = serializers.ChoiceField(choices=Profile.PROFILE_TYPE_CHOICES, default="IND")
 
     def validate_phone(self, value):
         phone_validator = PhoneNumberValidator(RUS)
@@ -47,9 +47,11 @@ class SignUpRequestSerializer(serializers.Serializer):
     @transaction.atomic
     def create(self, validated_data):
         phone = validated_data.get("phone", None)
-        first_name = validated_data.get("first_name", None)
-        email = validated_data.get("email", None)
         password = validated_data.get("password", None)
+        first_name = validated_data.get("first_name", None)
+
+        email = validated_data.get("email", None)
+
         profile_name = validated_data.get("profile_name", None)
         type = validated_data.get("type", None)
 
