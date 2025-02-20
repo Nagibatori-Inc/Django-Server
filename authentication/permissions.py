98563e913e675066e4f46f8658d66a3d626a0918
Daniil Lemeshko <gafret_kelp@outlook.com>
2025-02-20 21:32:10 +0300

    feat: create permission for accessing methods that can alter a profile

---
+++ authentication/permissions.py
@@ -0,0 +1,11 @@
+from rest_framework import permissions
+
+
+class IsProfileOwnerOrReadOnly(permissions.BasePermission):
+    """ Разрешение позволяющее менять профиль пользователя только самому пользователю """
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return not request.user.is_anonymous and request.user.profile == obj
