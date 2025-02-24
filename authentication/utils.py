c79c76217388a40ee1b2c11a309587a295bff2f9
Daniil Lemeshko <gafret_kelp@outlook.com>
2025-02-24 23:13:09 +0300

    chore: switch formatting logic, sms service require traditional country code (7)

--- authentication/utils.py
+++ authentication/utils.py
@@ -2,9 +2,9 @@
 
 def make_phone_uniform(phone: str) -> str:
     if phone.startswith("+7"):
-        return "8" + phone[2:]
-    elif phone.startswith("7"):
-        return "8" + phone[1:]
-    elif phone.startswith("+8"):
         return phone[1:]
+    elif phone.startswith("+8"):
+        return "7" + phone[2:]
+    elif phone.startswith("8"):
+        return "7" + phone[1:]
     return phone
