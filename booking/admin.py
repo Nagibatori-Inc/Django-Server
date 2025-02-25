3e854d76d6d5285122150b7fc40151e10e1c9f3e
DocLivsey <doclivsey@github.com>
2025-02-25 13:58:34 +0300

    feat: coded admin for adverts and promotions

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from booking.models import Advert, Promotion
+
+
+@admin.register(Advert)
+class AdvertAdmin(admin.ModelAdmin):
+    list_display = ['title', 'contact', 'price', 'created_at', 'activated_at', 'status']
+    search_fields = ['title', 'contact', 'price']
+    list_filter = ['created_at', 'activated_at', 'status']
+    readonly_fields = ['contact']
+    
+    
+@admin.register(Promotion)
+class PromotionAdmin(admin.ModelAdmin):
+    list_display = ['type', 'rate']
+    search_fields = ['type', 'rate']
+    list_filter = ['rate']
