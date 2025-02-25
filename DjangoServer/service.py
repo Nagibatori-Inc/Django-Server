ffdfa4119ae6208689b3c09cf307f1a0533af1b5
DocLivsey <doclivsey@users.noreply.github.com>
2025-02-26 00:09:56 +0300

    fix:  AttributeError: 'Response' object has no attribute 'or_else_send'

--- DjangoServer/service.py
+++ DjangoServer/service.py
@@ -13,6 +13,7 @@ class RestService:
     Fields:
         + response (Response): Необязательное поле,
         нужное для отправки ответа при публикации объявления или его изменения
+        + response_finalized (Bool): Флаг, который фиксирует момент, когда уже создан `response`
 
     Methods:
         + ok(): Если действия с объявлениями прошли успешно, возвращает `200 OK`, иначе продолжает цепочку
@@ -25,10 +26,12 @@ class RestService:
     Properties:
         + response(): Возвращает объект ответа
         + response(response): Сеттер для поля `response`
+        + response_finalized(): Возвращает приватное поле __response_finalized
     """
 
-    def __init__(self, response: Response = None):
+    def __init__(self, response: Response = None, response_finalized: bool = False):
         self.__response = response
+        self.__response_finalized = response_finalized
 
     @property
     def response(self):
@@ -37,6 +40,10 @@ class RestService:
     @response.setter
     def response(self, response: Response):
         self.__response = response
+
+    @property
+    def response_finalized(self):
+        return self.__response_finalized
 
     def _finalize_response(self):
         """
@@ -80,3 +87,20 @@ class RestService:
         :return: Response | RestService
         """
         return response() or Response(status=status_code)
+
+    def __getattr__(self, name):
+        """
+        Автоматически возвращает Response, если кто-то забыл вызвать or_else_send().
+        """
+        if self.response_finalized:
+            raise AttributeError(f"Response уже был создан, вызов `{name}` невозможен.")
+
+        return self.__dict__.get(name)
+
+    def __repr__(self):
+        """
+        Автоматически возвращает Response, если его забыли обработать.
+        """
+        return repr(self.response) \
+            if isinstance(self.response, Response) \
+            else super().__repr__()
