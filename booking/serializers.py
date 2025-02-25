ca7fe8900ea76f58ef1938f444ee69ba07ac6972
DocLivsey <doclivsey@users.noreply.github.com>
2025-02-25 23:51:06 +0300

    feat: temporary remove serializer on contact field, cause contact = request.user

--- booking/serializers.py
+++ booking/serializers.py
@@ -5,7 +5,6 @@ from authentication.serializers import ProfileSerializer
 
 
 class AdvertSerializer(serializers.ModelSerializer):
-    contact = ProfileSerializer(required=False)
     class Meta:
         model = Advert
         fields = ['title', 'description', 'price', 'phone']
