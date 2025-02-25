60ab74354f61579b39844e5c320e34963591f5db
DocLivsey <doclivsey@github.com>
2025-02-25 16:44:47 +0300

    feat: created nested serializer

--- booking/serializers.py
+++ booking/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 
 from booking.models import Advert
+from authentication.serializers import ProfileSerializer
 
 
 class AdvertSerializer(serializers.ModelSerializer):
+    contact = ProfileSerializer(required=False)
     class Meta:
         model = Advert
         fields = ['title', 'description', 'price', 'phone']
