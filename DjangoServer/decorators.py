086f691e965fcaba53a791fbe43fc90c80d99d6a
DocLivsey <doclivsey@users.noreply.github.com>
2025-02-26 13:39:03 +0300

    feat: trying to abstract exceptions and creation response via chainable handlers class

--- DjangoServer/decorators.py
+++ DjangoServer/decorators.py
@@ -2,6 +2,37 @@ from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import status
 from rest_framework.response import Response
 
+from DjangoServer.service import RestService
+
+
+class ServiceExceptionHandler:
+    def __init__(self, service: RestService):
+        self.__service = service
+
+    @property
+    def service(self):
+        return self.__service
+
+    @service.setter
+    def service(self, value):
+        self.__service = value
+
+    def wrap(self, method, *args, **kwargs):
+        return
+
+
+def handle_404(service, method, *args, **kwargs):
+    try:
+        return method(*args, **kwargs)
+
+    except ObjectDoesNotExist as e:
+        service.response = Response(
+            {'err_msg': str(e)},
+            status=status.HTTP_404_NOT_FOUND
+        )
+
+    return service
+
 
 def handle_service_exceptions(method):
     def wrapper(self, *args, **kwargs):
@@ -18,6 +49,3 @@ def handle_service_exceptions(method):
 
     return wrapper
 
-
-def handle_400(method):
-    pass
