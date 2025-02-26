282bdc4a842ef00ce4b6a0e80e9a20453e6fc564
DocLivsey <doclivsey@users.noreply.github.com>
2025-02-26 17:04:45 +0300

    feat: added serializer on promotion and appended some fields in search serializer

--- booking/serializers.py
+++ booking/serializers.py
@@ -1,16 +1,24 @@
 from rest_framework import serializers
 
-from booking.models import Advert
+from booking.models import Advert, Promotion
 from authentication.serializers import ProfileSerializer
 
 
+class PromotionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Promotion
+        fields = '__all__'
+
+
 class AdvertSerializer(serializers.ModelSerializer):
+    promotion = PromotionSerializer(required=False, read_only=True)
     class Meta:
         model = Advert
-        fields = ['title', 'description', 'price', 'phone']
+        fields = ['title', 'description', 'price', 'phone', 'promotion']
 
 
 class SearchFilterSerializer(serializers.ModelSerializer):
+    promotion = PromotionSerializer(required=False, read_only=True)
     class Meta:
         model = Advert
-        fields = ['title', 'price']
+        fields = ['title', 'price', 'promotion']
