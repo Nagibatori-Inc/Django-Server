93135595cb68f81758039cb9e770f93305b2d5e2
DocLivsey <doclivsey@users.noreply.github.com>
2025-02-27 12:19:33 +0300

    fix: improve imports

--- booking/serializers.py
+++ booking/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 
 from booking.models import Advert, Promotion
-from authentication.serializers import ProfileSerializer
 
 
 class PromotionSerializer(serializers.ModelSerializer):
