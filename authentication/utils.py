2bbe24a41858c1341225eca95577bbd2768d8500
Daniil Lemeshko <gafret_kelp@outlook.com>
2025-03-03 22:41:57 +0300

    feat: put some logic from sign up here

--- authentication/utils.py
+++ authentication/utils.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import User
+
+from authentication.models import Profile
 
 
 def make_phone_uniform(phone: str) -> str:
@@ -8,3 +11,33 @@ def make_phone_uniform(phone: str) -> str:
     elif phone.startswith("8"):
         return f"7{phone[1:]}"
     return phone
+
+
+def update_user(*, user: User, password: str, first_name: str, email: str, is_active: bool) -> None:
+    user.first_name = first_name
+    user.email = email
+    user.is_active = is_active
+    user.set_password(raw_password=password)
+    user.save(update_fields=["first_name", "email", "password"])
+
+
+def update_profile(*, profile: Profile, profile_name: str, profile_type: str, is_deleted: bool) -> None:
+    profile.name = profile_name
+    profile.type = profile_type
+    profile.is_deleted = is_deleted
+    profile.save(update_fields=["name", "type", "is_deleted"])
+
+
+def create_user(*, phone: str, password: str, first_name: str, email: str) -> User:
+    user = User(username=phone, first_name=first_name, email=email)
+    user.set_password(raw_password=password)
+    user.save()
+
+    return user
+
+
+def create_profile(*, user: User, profile_name: str, profile_type: str) -> Profile:
+    profile = Profile(user=user, name=profile_name, type=profile_type)
+    profile.save()
+
+    return profile
