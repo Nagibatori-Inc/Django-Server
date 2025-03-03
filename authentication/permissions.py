2d841343d3ac54b8487475c7a604936936772f3c
Daniil Lemeshko <gafret_kelp@outlook.com>
2025-03-03 22:56:10 +0300

    chore: add some types

--- authentication/permissions.py
+++ authentication/permissions.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.models import User
 from rest_framework import permissions
 
+from authentication.models import Profile
+
 
 class IsProfileOwnerOrReadOnly(permissions.BasePermission):
     """ Разрешение позволяющее менять профиль пользователя только самому пользователю """
 
-    def has_object_permission(self, request, view, obj):
+    def has_object_permission(self, request, view, obj: Profile) -> bool:
         if request.method in permissions.SAFE_METHODS:
             return True
 
