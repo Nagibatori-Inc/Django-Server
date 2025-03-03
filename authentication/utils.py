1ad62f3f100b54776be5ebc44c92f2b4baade1bb
Daniil Lemeshko <gafret_kelp@outlook.com>
2025-03-03 17:43:03 +0300

    perf: change concat to format string

--- authentication/utils.py
+++ authentication/utils.py
@@ -4,7 +4,7 @@ def make_phone_uniform(phone: str) -> str:
     if phone.startswith("+7"):
         return phone[1:]
     elif phone.startswith("+8"):
-        return "7" + phone[2:]
+        return f"7{phone[2:]}"
     elif phone.startswith("8"):
-        return "7" + phone[1:]
+        return f"7{phone[1:]}"
     return phone
