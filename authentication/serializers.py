bdfa8e43860b62a0d273d539b1fadebcde02734f
Daniil Lemeshko <gafret_kelp@outlook.com>
2025-03-03 22:39:07 +0300

    chore: rename serializers to more general ones

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -3,31 +3,23 @@ from rest_framework import serializers
 from authentication.models import Profile
 from authentication.misc.validators import validate_password, validate_ru_phone, validate_otp
 
-
+# Сделать два сериализатора для гет и для регистрации\данных своего профиля
 class ProfileSerializer(serializers.ModelSerializer):
+    # Добавить вложенного юзера
     class Meta:
         model = Profile
         fields = ["name", "type"]
 
 
-class PasswordResetDoneSerializer(serializers.Serializer):
+class PasswordSerializer(serializers.Serializer):
     password = serializers.CharField(required=True)
 
-    # for now password validation here, later will move it
     def validate_password(self, value):
         validate_password(value)
         return value
 
 
-class PasswordResetConfirmSerializer(serializers.Serializer):
-    otp_code = serializers.CharField(required=True, max_length=6)
-
-    def validate_otp_code(self, value):
-        validate_otp(value)
-        return value
-
-
-class PhonePasswordResetInitSerializer(serializers.Serializer):
+class PhoneSerializer(serializers.Serializer):
     phone = serializers.CharField(required=True, max_length=15)
 
     def validate_phone(self, value):
@@ -35,7 +27,7 @@ class PhonePasswordResetInitSerializer(serializers.Serializer):
         return value
 
 
-class EmailPasswordResetInitSerializer(serializers.Serializer):
+class EmailSerializer(serializers.Serializer):
     email = serializers.EmailField(required=True)
 
 
@@ -60,7 +52,7 @@ class SignUpRequestSerializer(serializers.Serializer):
     password = serializers.CharField(required=True)
     # Аттрибуты для профиля
     profile_name = serializers.CharField(required=False, max_length=50, allow_null=True)
-    type = serializers.ChoiceField(choices=Profile.PROFILE_TYPE_CHOICES, default="IND")
+    profile_type = serializers.ChoiceField(choices=Profile.PROFILE_TYPE_CHOICES, default="IND")
 
     def validate_phone(self, value: str) -> str:
         validate_ru_phone(value)
