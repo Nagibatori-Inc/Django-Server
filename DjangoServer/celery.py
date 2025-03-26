fac1351cbfd5b15fcac5dc23bfc922eecfb81f73
Daniil Lemeshko <gafret_kelp@outlook.com>
2025-03-26 19:12:36 +0300

    fix: reconfigure celery

--- DjangoServer/celery.py
+++ DjangoServer/celery.py
@@ -1,5 +1,8 @@
+import os
 from celery import Celery
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DjangoServer.settings")
+
 app = Celery("DjangoServer")
-app.config_from_object("django_conf:settings", namespace="CELERY")
+app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
