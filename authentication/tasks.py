5364c77fb0492fde38e2eddfce21808e606aed47
Daniil Lemeshko <gafret_kelp@outlook.com>
2025-03-26 19:24:50 +0300

    docs: change task name

--- authentication/tasks.py
+++ authentication/tasks.py
@@ -3,5 +3,5 @@ from authentication.services.sms import sms_service
 
 
 @celery_app.task
-def send_sms_wrapper(phone: str, text: str, **kwargs):
+def send_sms_task(phone: str, text: str, **kwargs):
     sms_service.send_message(phone, text, **kwargs)
