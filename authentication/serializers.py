b88971f892073792051d71c81dc3ffe61a41288b
Daniil Lemeshko <gafret_kelp@outlook.com>
2025-03-26 16:27:06 +0300

    feat: restucture serializers

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -3,48 +3,39 @@ from rest_framework import serializers
 from authentication.models import Profile
 from authentication.misc.validators import validate_password, validate_ru_phone, validate_otp
 
-# Сделать два сериализатора для гет и для регистрации\данных своего профиля
-class ProfileSerializer(serializers.ModelSerializer):
-    # Добавить вложенного юзера
-    class Meta:
-        model = Profile
-        fields = ["name", "type"]
+
+class PhoneValidationMixin:
+
+    def validate_phone(self, value):
+        validate_ru_phone(value)
+        return value
 
 
-class PasswordSerializer(serializers.Serializer):
-    password = serializers.CharField(required=True)
+class PasswordValidationMixin:
 
     def validate_password(self, value):
         validate_password(value)
         return value
 
 
-class PhoneSerializer(serializers.Serializer):
-    phone = serializers.CharField(required=True, max_length=15)
-
-    def validate_phone(self, value):
-        validate_ru_phone(value)
-        return value
+class PasswordSerializer(serializers.Serializer, PasswordValidationMixin):
+    password = serializers.CharField(required=True)
 
 
 class EmailSerializer(serializers.Serializer):
     email = serializers.EmailField(required=True)
 
 
-class VerificationRequestSerializer(serializers.Serializer):
+class VerificationRequestSerializer(serializers.Serializer, PhoneValidationMixin):
     phone = serializers.CharField(required=True, max_length=15)
     otp_code = serializers.CharField(required=True, max_length=6)
-
-    def validate_phone(self, value):
-        validate_ru_phone(value)
-        return value
 
     def validate_otp_code(self, value):
         validate_otp(value)
         return value
 
 
-class SignUpRequestSerializer(serializers.Serializer):
+class SignUpRequestSerializer(serializers.Serializer, PhoneValidationMixin, PasswordValidationMixin):
     # Аттрибуты для юзера
     phone = serializers.CharField(required=True, max_length=15)
     first_name = serializers.CharField(required=True)
@@ -54,13 +45,17 @@ class SignUpRequestSerializer(serializers.Serializer):
     profile_name = serializers.CharField(required=False, max_length=50, allow_null=True)
     profile_type = serializers.ChoiceField(choices=Profile.PROFILE_TYPE_CHOICES, default="IND")
 
-    def validate_phone(self, value: str) -> str:
-        validate_ru_phone(value)
-        return value
 
-    def validate_password(self, value: str) -> str:
-        validate_password(value)
-        return value
-
+class ProfileOwnerSerializer(serializers.ModelSerializer):
+    """Сериализатор, использующийся при удачном создании пользователя и при запросе данных о самом себе"""
+    class Meta:
+        model = Profile
+        fields = ["name", "type"]
 
 
+class ProfileSerializer(serializers.ModelSerializer):
+    """Сериализатор, используемый для получения данных о профиле иного (не себя) пользователя"""
+    # Добавить вложенного юзера
+    class Meta:
+        model = Profile
+        fields = ["name", "type"]
