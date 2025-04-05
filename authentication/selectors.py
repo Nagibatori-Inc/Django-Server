8cd2e055a1fe751739d103c2b5af9b80e0ca718a
Daniil Lemeshko <gafret_kelp@outlook.com>
2025-04-05 22:31:07 +0300

    feat: add otp getter

--- authentication/selectors.py
+++ authentication/selectors.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework.exceptions import NotFound
 
-from authentication.models import Profile
+from authentication.models import Profile, OneTimePassword
 
 
 def get_profile_with_user(pk: int = None) -> Profile:
@@ -22,3 +22,11 @@ def get_user_with_profile_by_phone(phone: str = None) -> User:
             "detail": "user not found"
         })
     return user
+
+
+def get_otp_with_user_by_code(token_code: str = None) -> OneTimePassword:
+    try:
+        otp = OneTimePassword.objects.select_related("user").get(code=token_code)
+    except OneTimePassword.DoesNotExist:
+        raise NotFound(detail={"detail": "otp not found"})
+    return otp
