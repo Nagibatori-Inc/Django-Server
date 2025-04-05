dfe6c079f453eebc09bd482c93e37d90f16c9dc4
Daniil Lemeshko <gafret_kelp@outlook.com>
2025-04-05 22:30:10 +0300

    refactor: remove shit

--- authentication/services/verification.py
+++ authentication/services/verification.py
@@ -6,17 +6,13 @@ from DjangoServer.settings import SMS_MODE, SMSAERO_API_KEY, SMSAERO_EMAIL
 from authentication.models import OneTimePassword
 from authentication.services.sms import BaseSmsService, SmsAeroService, SmsDebugService
 
-sms_service = SmsDebugService()
-if SMS_MODE == "production":
-    sms_service = SmsAeroService(api_key=SMSAERO_API_KEY, email=SMSAERO_EMAIL)
-
 
 class BaseVerificationService:
     """
     Сервис, отвечающий за верификацию пользователя
 
     Methods:
-        + verify: При условии действительности пользователя, ставит в профиле поле is_verified = True
+        + verify:
     """
     def __init__(self, user: User):
         self.user = user
@@ -39,30 +35,9 @@ class BaseVerificationService:
         if not otp_valid:
             raise ValidationError(detail={"detail": "otp doesn't match"})
 
-    def _create_otp(self) -> str:
+    def create_otp(self) -> str:
         otp = OneTimePassword(user=self.user).save()
         return otp
 
-    def send_otp(self, message_template: str) -> None:
-        raise NotImplementedError("Should be implemented in children classes")
-
     def verify_otp(self, otp: str) -> None:
         self._validate_otp(otp)
-
-
-class SmsVerificationService(BaseVerificationService):
-    sms_service: BaseSmsService = sms_service
-
-    def __init__(self, user: User):
-        super().__init__(user)
-
-    def send_otp(self, message_template: str) -> None:
-        otp = self._create_otp()
-        message = message_template.format(otp)
-        phone_number = self.user.username
-
-        self.sms_service.send_message(phone_number, message)
-
-
-class EmailVerificationService(BaseVerificationService):
-    pass
