162054c9c84063651fa040f540c6fa68ddfff9f3
DocLivsey <doclivsey@github.com>
2025-04-07 15:53:08 +0300

    fix: missing spacing

--- DjangoServer/helpers/datetime.py
+++ DjangoServer/helpers/datetime.py
@@ -5,5 +5,6 @@ from datetime import datetime, timedelta
 def get_month_length(date: datetime):
     return calendar.monthrange(date.year, date.month)[1]
 
+
 def renew_for_month(date: datetime):
     return date + timedelta(days=get_month_length(date))
