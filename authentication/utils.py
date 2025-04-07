0abdee9c833c9fa561dfbcd4a1558f49cd005436
Daniil Lemeshko <gafret_kelp@outlook.com>
2025-04-07 13:46:14 +0300

    refactor: move functions back to class

--- authentication/utils.py
+++ authentication/utils.py
@@ -1,9 +1,7 @@
-from django.contrib.auth.models import User
-
 from authentication.exceptions import InvalidPhoneError
-from authentication.models import Profile
 
 
+# TODO сделать так, чтобы телефон приводился к ЗАДАННОМУ стандарту, а не захардкоженному
 def make_phone_uniform(phone: str) -> str:
     """ Унификатор телефонного номера, приводит его к формату 71234567890"""
     if phone.startswith("+7"):
@@ -17,32 +15,3 @@ def make_phone_uniform(phone: str) -> str:
     else:
         raise InvalidPhoneError("Phone starts with invalid code")
 
-
-def update_user(*, user: User, password: str, first_name: str, email: str, is_active: bool) -> None:
-    user.first_name = first_name
-    user.email = email
-    user.is_active = is_active
-    user.set_password(raw_password=password)
-    user.save(update_fields=["first_name", "email", "password"])
-
-
-def update_profile(*, profile: Profile, profile_name: str, profile_type: str, is_deleted: bool) -> None:
-    profile.name = profile_name
-    profile.type = profile_type
-    profile.is_deleted = is_deleted
-    profile.save(update_fields=["name", "type", "is_deleted"])
-
-
-def create_user(*, phone: str, password: str, first_name: str, email: str) -> User:
-    user = User(username=phone, first_name=first_name, email=email)
-    user.set_password(raw_password=password)
-    user.save()
-
-    return user
-
-
-def create_profile(*, user: User, profile_name: str, profile_type: str) -> Profile:
-    profile = Profile(user=user, name=profile_name, type=profile_type)
-    profile.save()
-
-    return profile
