b8426df23f72c29473f6e984f64b43f15e18fba0
Daniil Lemeshko <gafret_kelp@outlook.com>
2025-04-07 12:52:23 +0300

    feat: add get_my_profile

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,8 @@
+from knox.auth import TokenAuthentication
 from knox.views import LoginView as KnoxLoginView
 from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -7,15 +10,14 @@ from rest_framework.viewsets import ViewSet
 
 from authentication.misc.custom_auth import CustomBasicAuthentication
 from authentication.permissions import IsProfileOwnerOrReadOnly
-from authentication.selectors import get_profile_with_user, get_user_with_profile_by_phone, get_otp_with_user_by_code
+from authentication.selectors import get_profile_with_user, get_user_with_profile_by_phone
 from authentication.serializers import ProfileSerializer, SignUpRequestSerializer, VerificationRequestSerializer, \
-    PhoneSerializer, PasswordResetSerializer
+    PhoneSerializer, PasswordResetSerializer, ProfileOwnerSerializer
 from authentication.services.profile import ProfileManagerService, PasswordManagerService
 from authentication.services.verification import BaseVerificationService
 from authentication.tasks import send_sms_task
 from authentication.utils import make_phone_uniform
-
-MESSAGE_TEMPLATE = "Зубастый ректум приглашает вас, код {0}"
+from DjangoServer.settings import MESSAGE_TEMPLATE
 
 
 class LoginView(KnoxLoginView):
@@ -23,6 +25,7 @@ class LoginView(KnoxLoginView):
 
 
 class ProfileVerificationView(APIView):
+    """View, использующийся для верификации профиля пользователя по OTP коду"""
     def post(self, request, *args, **kwargs):
         serializer = VerificationRequestSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -39,6 +42,7 @@ class ProfileVerificationView(APIView):
 
 
 class SendVerificationCodeView(APIView):
+    """View, использующийся для отправки OTP кода пользователю"""
     def post(self, request):
         serializer = PhoneSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -52,6 +56,7 @@ class SendVerificationCodeView(APIView):
 
 
 class ResetPasswordValidateTokenView(APIView):
+    """View, использующийся для валидации OTP кода, без каких либо побочных эффектов"""
     def post(self, request: Request):
         serializer = VerificationRequestSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -66,17 +71,17 @@ class ResetPasswordValidateTokenView(APIView):
 
 
 class ResetPasswordConfirmView(APIView):
+    """View, использующийся для смены пароля"""
     def post(self, request: Request):
         serializer = PasswordResetSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
+        phone = make_phone_uniform(serializer.validated_data.get("phone"))
         password = serializer.validated_data.get("password")
-        otp_code = serializer.validated_data.get("otp_code")
+        otp = serializer.validated_data.get("otp_code")
+        user = get_user_with_profile_by_phone(phone)
 
-        otp = get_otp_with_user_by_code(otp_code)
-        user = otp.user
-
-        BaseVerificationService(user).verify_otp(otp_code)
+        BaseVerificationService(user).verify_otp(otp)
         PasswordManagerService(user).reset_password(new_password=password)
 
         return Response({"detail": "password changed"}, status=status.HTTP_200_OK)
@@ -99,30 +104,33 @@ class SignUpView(APIView):
 class ProfileViewSet(ViewSet):
     permission_classes = [IsProfileOwnerOrReadOnly]
 
-    def retrieve(self, request: Request, pk: int = None):
+    @action(detail=False, methods=['get'], permission_classes=(IsAuthenticated, ), url_path='my_profile')
+    def get_my_profile(self, request: Request):
+        profile = request.user.profile
+        serialized_profile = ProfileOwnerSerializer(profile).data
+
+        return Response(serialized_profile, status=status.HTTP_200_OK)
+
+    def retrieve(self, request: Request, pk: int | None = None):
         profile = get_profile_with_user(pk)
         serialized_profile = ProfileSerializer(profile).data
 
-        return Response({
-            "profile": serialized_profile
-        }, status=status.HTTP_200_OK)
+        return Response(serialized_profile, status=status.HTTP_200_OK)
 
-    def update(self, request: Request, pk: int = None):
+    def update(self, request: Request, pk: int | None = None):
         profile = get_profile_with_user(pk)
         self.check_object_permissions(request, profile)
 
-        serializer = ProfileSerializer(data=request.data)
+        serializer = ProfileOwnerSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
         ProfileManagerService(profile).update(**serializer.validated_data)
 
-        serialized_profile = ProfileSerializer(profile).data
+        serialized_profile = ProfileOwnerSerializer(profile).data
 
-        return Response({
-            "profile": serialized_profile
-        }, status=status.HTTP_200_OK)
+        return Response(serialized_profile, status=status.HTTP_200_OK)
 
-    def destroy(self, request: Request, pk: int = None):
+    def destroy(self, request: Request, pk: int | None = None):
         profile = get_profile_with_user(pk)
         self.check_object_permissions(request, profile)
 
