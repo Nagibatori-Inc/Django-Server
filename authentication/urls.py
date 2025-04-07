a62d8421d8af08a934cdf2a6456885bdbfd94b9b
Daniil Lemeshko <gafret_kelp@outlook.com>
2025-04-07 12:53:10 +0300

    feat: add paths

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,7 +3,7 @@ from knox.views import LogoutView, LogoutAllView
 from rest_framework.routers import DefaultRouter
 
 from authentication.views import SignUpView, ProfileViewSet, LoginView, ProfileVerificationView, \
-    SendVerificationCodeView
+    SendVerificationCodeView, ResetPasswordValidateTokenView, ResetPasswordConfirmView
 
 router = DefaultRouter()
 router.register(r"profiles", ProfileViewSet, basename="profile")
@@ -13,6 +13,8 @@ urlpatterns = [
     path("auth/logout_all/", LogoutAllView.as_view(), name="logout-all"),
     path("auth/sign_up/", SignUpView.as_view(), name="sign-up"),
     path("auth/send_code/", SendVerificationCodeView.as_view(), name="send-code"),
-    path("auth/verify/", ProfileVerificationView.as_view(), name="otp-verification"),
+    path("auth/verify/", ProfileVerificationView.as_view(), name="profile-verification"),
+    path("auth/validate/", ResetPasswordValidateTokenView.as_view(), name="otp-validation"),
+    path("auth/reset_password/", ResetPasswordConfirmView.as_view(), name="password-reset"),
     path("", include(router.urls))
 ]
