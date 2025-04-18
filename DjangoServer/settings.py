7663046730a032256c612a697d0226fe1c51a923
DocLivsey <doclivsey@users.noreply.github.com>
2025-04-19 00:08:39 +0300

    feat: ok, fuck caddy. do django cors brrrr brrr

--- DjangoServer/settings.py
+++ DjangoServer/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'authentication.apps.AuthenticationConfig',
     "django_migration_linter",
     'drf_spectacular',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -60,7 +61,11 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = "DjangoServer.urls"
 
