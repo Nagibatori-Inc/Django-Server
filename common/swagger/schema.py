d222a90a688c3a079a3b8b1c8aaa6bf32063de17
Vladimir Chernov <chernovvova2023@yandex.ru>
2025-04-21 00:43:29 +0300

    refactor: add constants for public APIs

--- common/swagger/schema.py
+++ common/swagger/schema.py
@@ -1,18 +1,29 @@
 from drf_spectacular.utils import OpenApiResponse
 from rest_framework import status
 
+SWAGGER_NO_RESPONSE_BODY = 'No response body'
+
 AUTH_ERRORS_SCHEMA_RESPONSES = {
     status.HTTP_401_UNAUTHORIZED: OpenApiResponse(description='Unauthorized'),
     status.HTTP_403_FORBIDDEN: OpenApiResponse(description='Forbidden'),
 }
 
-DEFAULT_ERRORS_SCHEMA_RESPONSES = {
-    **AUTH_ERRORS_SCHEMA_RESPONSES,
+DEFAULT_PUBLIC_API_SCHEMA_RESPONSES = {
     status.HTTP_400_BAD_REQUEST: OpenApiResponse(description='Bad Request'),
     status.HTTP_500_INTERNAL_SERVER_ERROR: OpenApiResponse(description='Internal Server Error'),
 }
 
-DEFAULT_ERRORS_WITH_404_SCHEMA_RESPONSES = {
-    **DEFAULT_ERRORS_SCHEMA_RESPONSES,
+DEFAULT_PRIVATE_ERRORS_SCHEMA_RESPONSES = {
+    **AUTH_ERRORS_SCHEMA_RESPONSES,
+    **DEFAULT_PUBLIC_API_SCHEMA_RESPONSES,
+}
+
+DEFAULT_PUBLIC_ERRORS_WITH_404_SCHEMA_RESPONSES = {
+    **DEFAULT_PUBLIC_API_SCHEMA_RESPONSES,
+    status.HTTP_404_NOT_FOUND: OpenApiResponse(description='Not Found'),
+}
+
+DEFAULT_PRIVATE_ERRORS_WITH_404_SCHEMA_RESPONSES = {
+    **DEFAULT_PRIVATE_ERRORS_SCHEMA_RESPONSES,
     status.HTTP_404_NOT_FOUND: OpenApiResponse(description='Not Found'),
 }
