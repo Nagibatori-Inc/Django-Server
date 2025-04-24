4ef18e8f9fee25f6e9df160d83e34389e2b9e833
Vladimir Chernov <chernovvova2023@yandex.ru>
2025-04-24 23:07:59 +0300

    feat: remove custom logout views

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from knox.views import LogoutView, LogoutAllView
 from rest_framework.routers import DefaultRouter
 
 from authentication.views import (
@@ -9,16 +10,14 @@ from authentication.views import (
     SendVerificationCodeView,
     ResetPasswordValidateTokenView,
     ResetPasswordConfirmView,
-    UserLogoutAllView,
-    UserLogoutView,
 )
 
 router = DefaultRouter()
 router.register(r"profiles", ProfileViewSet, basename="profile")
 urlpatterns = [
     path("auth/login/", LoginView.as_view(), name="login"),
-    path("auth/logout/", UserLogoutView.as_view(), name="logout"),
-    path("auth/logout_all/", UserLogoutAllView.as_view(), name="logout-all"),
+    path("auth/logout/", LogoutView.as_view(), name="logout"),
+    path("auth/logout_all/", LogoutAllView.as_view(), name="logout-all"),
     path("auth/sign_up/", SignUpView.as_view(), name="sign-up"),
     path("auth/send_code/", SendVerificationCodeView.as_view(), name="send-code"),
     path("auth/verify/", ProfileVerificationView.as_view(), name="profile-verification"),
--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,6 @@
 from drf_spectacular.utils import extend_schema, inline_serializer, OpenApiResponse, OpenApiExample
 from knox.auth import TokenAuthentication
-from knox.views import LoginView as KnoxLoginView, LogoutAllView, LogoutView
+from knox.views import LoginView as KnoxLoginView
 from rest_framework import status, serializers
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated, AllowAny
@@ -33,32 +33,6 @@ from common.swagger.schema import (
 
 AUTHENTIFICATION_SWAGGER_TAG = 'Авторизация'
 PROFILE_SWAGGER_TAG = 'Профили'
-
-
-@extend_schema(
-    tags=[AUTHENTIFICATION_SWAGGER_TAG],
-    description='Выйти',
-    request={},
-    responses={
-        status.HTTP_204_NO_CONTENT: SWAGGER_NO_RESPONSE_BODY,
-        **DEFAULT_PRIVATE_API_ERRORS_SCHEMA_RESPONSES,
-    },
-)
-class UserLogoutView(LogoutView):
-    pass
-
-
-@extend_schema(
-    tags=[AUTHENTIFICATION_SWAGGER_TAG],
-    description='Выйти из всех сессий',
-    request={},
-    responses={
-        status.HTTP_204_NO_CONTENT: SWAGGER_NO_RESPONSE_BODY,
-        **DEFAULT_PRIVATE_API_ERRORS_SCHEMA_RESPONSES,
-    },
-)
-class UserLogoutAllView(LogoutAllView):
-    pass
 
 
 @extend_schema(
