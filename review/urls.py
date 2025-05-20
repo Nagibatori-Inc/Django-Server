46a97f83b46363157605cf44b6b32ef9e63206ca
Vladimir Chernov <chernovvova2023@yandex.ru>
2025-05-20 20:25:40 +0300

    feat: add urls for DeleteReviewAPIView

--- review/urls.py
+++ review/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from review.views import ProfileReviewsListAPIView, ProfileReviewsAPIView
+from review.views import ProfileReviewsListAPIView, CreateReviewAPIView, DeleteReviewAPIView
 
 urlpatterns = [
     path('profiles/<int:profile_id>/reviews/', ProfileReviewsListAPIView.as_view(), name='profile_reviews'),
-    path('profiles/<int:profile_id>/reviews/', ProfileReviewsAPIView.as_view(), name='profile_reviews'),
+    path('profiles/<int:profile_id>/reviews/', CreateReviewAPIView.as_view(), name='create_review'),
+    path('profiles/<int:profile_id>/reviews/<int:review_id>/', DeleteReviewAPIView.as_view(), name='delete_review'),
 ]
