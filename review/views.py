1ac65861bf37a3d2ded7ed8ef1befa0a732e49e9
Vladimir Chernov <chernovvova2023@yandex.ru>
2025-05-20 18:20:33 +0300

    feat: add view for creating and deleting reviews

--- review/views.py
+++ review/views.py
@@ -1,12 +1,15 @@
 from drf_spectacular.utils import extend_schema, OpenApiResponse
 from rest_framework import status
+from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from authentication.misc.custom_auth import CookieTokenAuthentication
+from authentication.selectors.profile import get_profile_by_id
 from review.selectors.review import get_visible_reviews
 from review.serializers import ReviewSerializer
-
+from review.services.review import delete_review_by_id
 
 SWAGGER_REVIEWS_TAG = 'Отзывы'
 
@@ -31,3 +34,40 @@ class ProfileReviewsListAPIView(APIView):
         """Получить отзывы профиля"""
         queryset = get_visible_reviews(profile_id)
         return Response(status=status.HTTP_200_OK, data=self.serializer_class(queryset, many=True).data)
+
+
+@extend_schema(tags=[SWAGGER_REVIEWS_TAG])
+class ProfileReviewsAPIView(CreateAPIView):
+    """Вью для оставления и удаления отзывов"""
+
+    authentication_classes = [CookieTokenAuthentication]
+    permission_classes = [AllowAny]
+    serializer_class = ReviewSerializer
+
+    def post(self, request, profile_id: int):
+        """Создание отзыва"""
+        profile = get_profile_by_id(profile_id)
+
+        if request.user.profile.id == profile.id:  # type: ignore[attr-defined]
+            return Response(
+                data={'error_detail': 'Нельзя оставить отзыв самому себе'}, status=status.HTTP_422_UNPROCESSABLE_ENTITY
+            )
+
+        serializer = self.serializer_class(data={'profile_id': profile.id, **request.data})  # type: ignore[attr-defined]
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(status=status.HTTP_201_CREATED)
+
+    def delete(self, request, profile_id: int, review_id: int):
+        """Удаление отзыва"""
+        profile = get_profile_by_id(profile_id)
+
+        if request.user.profile.id != profile.id:  # type: ignore[attr-defined]
+            return Response(
+                data={'error_detail': 'Нельзя удалить чужой отзыв'}, status=status.HTTP_422_UNPROCESSABLE_ENTITY
+            )
+
+        delete_review_by_id(review_id)
+
+        return Response(status=status.HTTP_200_OK)
