4fd11502c05ae602ecb2d58d70fa531d696ecd97
Vladimir Chernov <chernovvova2023@yandex.ru>
2025-05-20 18:20:54 +0300

    feat: add urls for ProfileReviewsAPIView

--- review/urls.py
+++ review/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from review.views import ProfileReviewsListAPIView
+from review.views import ProfileReviewsListAPIView, ProfileReviewsAPIView
 
 urlpatterns = [
-    path('profiles/<int:profile_id>/reviews', ProfileReviewsListAPIView.as_view(), name='profile_reviews'),
+    path('profiles/<int:profile_id>/reviews/', ProfileReviewsListAPIView.as_view(), name='profile_reviews'),
+    path('profiles/<int:profile_id>/reviews/', ProfileReviewsAPIView.as_view(), name='profile_reviews'),
 ]
