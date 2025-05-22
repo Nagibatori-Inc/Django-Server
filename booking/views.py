d62e2c7d8647641516dfb1429e49dce4adb2956a
Vladimir Chernov <chernovvova2023@yandex.ru>
2025-05-22 20:52:15 +0300

    feat: ignore mypy errors

--- booking/views.py
+++ booking/views.py
@@ -39,7 +39,7 @@ class AdvertViewSet(ViewSet):
         },
     )
     def list(self, request):
-        profile: Profile = get_object_or_404(Profile, user=request.user)
+        profile: Profile = get_object_or_404(Profile, user=request.user)  # type: ignore[annotation-unchecked]
         return (
             AdvertsRecommendationService(self.queryset.filter(contact=profile))
             .serialize(self.serializer_class)
