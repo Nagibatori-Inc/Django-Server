b95b5696790ecbea25067289aa9584cd09fe8f5a
Vladimir Chernov <chernovvova2023@yandex.ru>
2025-05-22 20:14:37 +0300

    feat: add service for review moderation

--- review/services/review.py
+++ review/services/review.py
@@ -23,3 +23,12 @@ def recalc_profile_rating(profile_id: int) -> None:
     """
     new_rating = Review.objects.filter(is_approved=True).aggregate(Avg('rate'))['rate__avg']
     Profile.objects.filter(id=profile_id).update(rating=new_rating)
+
+
+def moderate_review(review: Review, is_approved: bool) -> None:
+    """Модерация отзыва"""
+    if is_approved:
+        Review.objects.filter(pk=review.pk).update(is_approved=is_approved)
+    else:
+        Review.objects.filter(pk=review.pk).delete()
+    recalc_profile_rating(profile_id=review.profile.id)
