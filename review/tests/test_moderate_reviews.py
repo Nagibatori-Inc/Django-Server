b3b2b1ae46115b11586ff18ca27ad019e49f8cb4
Vladimir Chernov <chernovvova2023@yandex.ru>
2025-05-24 20:38:52 +0300

    feat(review test): remove test error pre-commit pytest hook is working now!

--- review/tests/test_moderate_reviews.py
+++ review/tests/test_moderate_reviews.py
@@ -46,7 +46,7 @@ class TestModerateReviews:
 
         response = auth_client.get(reverse(self.moderate_review_url_name))
 
-        assert response.status_code == status.HTTP_200_OK + 11
+        assert response.status_code == status.HTTP_200_OK
 
     def test_unauthorized_moderate_review_request(self, api_client: APIClient):
         """
