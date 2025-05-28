93e8612d0040a7a3ba4780d67741cd32279bee38
Daniil Lemeshko <gafret_kelp@outlook.com>
2025-05-28 15:44:30 +0300

    feat: handle webhook

--- payments/views.py
+++ payments/views.py
@@ -1,10 +1,13 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK
+from rest_framework.status import HTTP_200_OK, HTTP_409_CONFLICT
 from rest_framework.views import APIView
 
 from authentication.misc.custom_auth import CookieTokenAuthentication
+from booking.permissions import IsAdvertOwnerOrReadOnly
+from booking.services import PromotionService
+from payments.models import PaymentStatus
 from payments.selectors import get_payment_by_external_id
 from payments.serializers import PaymentSerializer, WebHookEventSerializer
 from payments.services.purchase_processors import YooKassa
@@ -12,11 +15,18 @@ from payments.services.purchase_processors import YooKassa
 
 class PromotionPurchaseView(APIView):
     authentication_classes = [CookieTokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsAdvertOwnerOrReadOnly]
 
     def post(self, request: Request):
         payment_data = PaymentSerializer(data=request.data)
         payment_data.is_valid(raise_exception=True)
+
+        advert = payment_data.validated_data["advert"]
+
+        self.check_object_permissions(request, advert)
+
+        if advert.is_promoted:
+            return Response(status=HTTP_409_CONFLICT)
 
         payment = payment_data.save(user=request.user)
         confirm_url = YooKassa(payment).init_transaction()
@@ -34,5 +44,7 @@ class PaymentSystemWebHookView(APIView):
         internal_payment = get_payment_by_external_id(external_payment["id"])
 
         YooKassa(internal_payment).finalize_transaction(event, external_payment)
+        if internal_payment.status == PaymentStatus.SUCCESSFUL:
+            PromotionService().promote("Базовое", 1, internal_payment.advert)
 
         return Response(status=HTTP_200_OK)
