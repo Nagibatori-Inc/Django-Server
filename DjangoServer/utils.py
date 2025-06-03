bc592f93076b22ad5581dc10fde29703d32d5bf2
DocLivsey <doclivsey@users.noreply.github.com>
2025-06-04 02:12:48 +0300

    fix: advert does have contact field instead of user

--- DjangoServer/utils.py
+++ DjangoServer/utils.py
@@ -1,2 +1,2 @@
 def user_directory_path(instance, filename):
-    return 'adverts/images/user_{0}/{1}'.format(instance.user.id, filename)
+    return 'adverts/images/user_{0}/{1}'.format(instance.contact.id, filename)
