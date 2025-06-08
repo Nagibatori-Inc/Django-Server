4ba06334e44ee4c8b2f6fea9562f4008740f3bd3
Daniil Lemeshko <gafret_kelp@outlook.com>
2025-06-08 12:17:38 +0300

    docs: add open api specs

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,5 @@
+from drf_spectacular.utils import extend_schema, inline_serializer
+from rest_framework import status, serializers
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -7,12 +9,28 @@ from rest_framework.views import APIView
 from authentication.misc.custom_auth import CookieTokenAuthentication
 from booking.permissions import IsAdvertOwnerOrReadOnly
 from booking.services import PromotionService
+from common.swagger.schema import SWAGGER_NO_RESPONSE_BODY, DEFAULT_PUBLIC_API_SCHEMA_RESPONSES
 from payments.models import PaymentStatus
 from payments.selectors import get_payment_by_external_id
 from payments.serializers import PaymentSerializer, WebHookEventSerializer
 from payments.services.purchase_processors import YooKassa
 
 
+PAYMENTS_SWAGGER_TAG = "Оплата"
+
+
+@extend_schema(
+    tags=[PAYMENTS_SWAGGER_TAG],
+    description='Покупка продвижения',
+    request=WebHookEventSerializer,
+    responses={
+        status.HTTP_200_OK: inline_serializer(
+            name='PaymentResponse',
+            fields={'confirmation_url': serializers.CharField()},
+        ),
+        **DEFAULT_PUBLIC_API_SCHEMA_RESPONSES,
+    },
+)
 class PromotionPurchaseView(APIView):
     authentication_classes = [CookieTokenAuthentication]
     permission_classes = [IsAuthenticated, IsAdvertOwnerOrReadOnly]
@@ -34,6 +52,16 @@ class PromotionPurchaseView(APIView):
         return Response(status=HTTP_200_OK, data={"confirmation_url": confirm_url})
 
 
+@extend_schema(
+    tags=[PAYMENTS_SWAGGER_TAG],
+    description='Вебхук для уведомлений от банка',
+    request=WebHookEventSerializer,
+    responses={
+        status.HTTP_200_OK: SWAGGER_NO_RESPONSE_BODY,
+        status.HTTP_500_INTERNAL_SERVER_ERROR: SWAGGER_NO_RESPONSE_BODY,
+        **DEFAULT_PUBLIC_API_SCHEMA_RESPONSES,
+    },
+)
 class PaymentSystemWebHookView(APIView):
     def post(self, request: Request):
         webhook_data = WebHookEventSerializer(data=request.data)
