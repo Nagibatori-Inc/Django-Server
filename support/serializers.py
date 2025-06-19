3a46548fcc48c163361edf93f269c57208e2b65c
Daniil Lemeshko <gafret_kelp@outlook.com>
2025-06-19 12:45:00 +0300

    feat: add default serializers

--- support/serializers.py
+++ support/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from support.models import SupportMessage
+from support.models import SupportMessage, SupportAnswer
 
 
 class SupportMessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = SupportMessage
         fields = ["subject", "message"]
+
+
+class SupportAnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SupportAnswer
+        fields = ["answer"]
