df8cdf7c7e6b9ed1b36d530af6abe38d88ec41e9
Daniil Lemeshko <gafret_kelp@outlook.com>
2025-06-19 13:13:58 +0300

    feat: add message selector by id

--- support/selectors/messages.py
+++ support/selectors/messages.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from rest_framework.exceptions import ValidationError
 
 from support.models import SupportMessage
 
@@ -7,3 +8,12 @@ def get_user_messages(user: User):
     support_messages = SupportMessage.objects.filter(user=user)
 
     return support_messages
+
+
+def get_message_by_id(message_id: int):
+    try:
+        message = SupportMessage.objects.get(pk=message_id)
+    except SupportMessage.DoesNotExist:
+        raise ValidationError("Нет такого сообщения")
+
+    return message
