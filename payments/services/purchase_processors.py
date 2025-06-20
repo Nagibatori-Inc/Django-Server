ac2c29a537ad183409448a9f99b9c926f79880fe
Daniil Lemeshko <gafret_kelp@outlook.com>
2025-06-20 17:15:33 +0300

    feat: add transaction verification

--- payments/services/purchase_processors.py
+++ payments/services/purchase_processors.py
@@ -3,7 +3,7 @@ import datetime
 
 import requests
 import structlog
-from rest_framework.exceptions import ValidationError
+from rest_framework.exceptions import ValidationError, APIException
 
 from DjangoServer.settings import YOO_KASSA_ID, YOO_KASSA_SECRET
 from payments.exceptions import PaymentError
@@ -23,6 +23,7 @@ class YooKassa:
     PAYMENT_URL = "https://api.yookassa.ru/v3/payments/"
 
     def __init__(self, payment: Payment):
+        print(YOO_KASSA_ID, YOO_KASSA_SECRET)
         self.payment = payment
         self.headers = {
             "Idempotence-Key": str(payment.id),
@@ -40,12 +41,16 @@ class YooKassa:
                 json=data,
                 headers=self.headers,
             )
+            transaction_data = response.json()
+
+            if transaction_data.get("error") == "error":
+                raise APIException("Something wrong with request to yookassa")
+
         except Exception as e:
             logger.error("Error during request to payment gateway", error=e)
             raise PaymentError(detail="Ошибка при отправке запроса в платежную систему")
 
         try:
-            transaction_data = response.json()
             transaction_id = transaction_data["id"]
             confirm_url = transaction_data["confirmation"]["confirmation_url"]
         except Exception as e:
@@ -59,10 +64,12 @@ class YooKassa:
 
     def finalize_transaction(self, event_type: str, external_payment: dict):
         try:
-            response = requests.get(
-                url=self.PAYMENT_URL + external_payment["id"],
-            )
+            response = requests.get(url=self.PAYMENT_URL + external_payment["id"], headers=self.headers)
             verified_transaction = response.json()
+
+            if verified_transaction.get(type) == "error":
+                raise APIException("Something wrong with request to yookassa")
+
         except Exception as e:
             logger.error("Error during request for verification of transaction", error=e)
             raise PaymentError(detail="Ошибка при запросе на проверку транзакции")
@@ -75,7 +82,8 @@ class YooKassa:
             raise PaymentError(detail="Ошибка обработки вебхука")
 
         if verified_transaction["status"] != external_status:
-            raise ValidationError("Транзакция невверна")
+            logger.info("Error during comparison of webhook and api transactions")
+            raise ValidationError("Транзакция неверна")
 
         self.payment.status = internal_status
         self.payment.completed_at = datetime.datetime.now()
