345ec31572c5007a06d243f13bda3d36372144be
Daniil Lemeshko <gafret_kelp@outlook.com>
2025-06-20 17:29:22 +0300

    fix: reformat file

--- booking/permissions.py
+++ booking/permissions.py
@@ -4,7 +4,6 @@ from booking.models import Advert
 
 
 class IsAdvertOwnerOrReadOnly(permissions.BasePermission):  # type: ignore[misc]
-
     def has_object_permission(self, request, view, obj: Advert) -> bool:
         if request.method in permissions.SAFE_METHODS:
             return True
