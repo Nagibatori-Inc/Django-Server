14a266acc6d5c40a7cf8ed36dcb642250f943761
Daniil Lemeshko <gafret_kelp@outlook.com>
2025-06-22 21:20:15 +0300

    feat: kruto sdelal

--- notification/tasks.py
+++ notification/tasks.py
@@ -8,6 +8,5 @@ def send_email_task(subject: str, html_message: str, email: str):
     send_mail(subject=subject, html_message=html_message, email=email)
 
 
-@celery_app.task
 def send_sms_task(phone: str, text: str, **kwargs):
     sms_service.send_message(phone, text, **kwargs)
